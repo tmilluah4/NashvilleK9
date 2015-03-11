8ce99a8f35acb262bd6e27274329ac17911145b9
tmilluah4 <todd.miller3@yahoo.com>
2015-03-11 14:57:00 -0500

    Home Page - first commit

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Nashville K-9</title>
+    <meta name="viewport" http-equiv="Content-Type" content="width=device-width, intitial-scale=1.0" />
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/styles.css" rel="stylesheet">
+	<link href="css/img-hover-effect.css" rel="stylesheet"> 
+	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
+	 
+</script>
+</head>
+<body>
+	 	
+    <?php include('header.html');?> 	
+     
+    <div class="main">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 pull-left "> 
+					<div class="video-box"> </div>
+					<div class="video">
+							<iframe src="http://player.vimeo.com/video/21218268?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="560" height="310" style="border:0px" frameborder="0" allowtransparency="true"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+					</div>
+                </div>
+
+                <div class="col-md-6 pull-right mainText">
+                    <blockquote style="line-height: 1.8em;  ">
+                        We cover a wide variety of needs at Nashville K-9: Search and Rescue, Executive Protection, we even train dogs for law enforcement agencies. However the most important thing we do is address the needs of your house pets. From day one we implement techniques like luring, imprinting, and proofing to make sure your new family member starts out with a strong foundation of obedience and trainability. Our techniques are practical, fun, and will impress your neighbors. With a lifetime guarantee, you can rest assured that we stand behind our training.
+                    </blockquote>
+                </div>
+            </div>
+        </div>
+    </div>	
+	
+	<?php include('lower-main.html');?> 	
+	<?php include('footer.html');?> 	
+   
+</body>
+</html>
