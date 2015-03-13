842d84941614bf4f81500134db1b9bfa8193c928
tmilluah4 <todd.miller3@yahoo.com>
2015-03-13 17:04:38 -0500

    more changes to home page and partials

--- index.php
+++ index.php
@@ -3,31 +3,48 @@
 <head> 
     <title>Nashville K-9</title>
     <meta name="viewport" http-equiv="Content-Type" content="width=device-width, intitial-scale=1.0" />
+	  <meta name="format-detection" content="telephone=no" />
     <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/styles.css" rel="stylesheet">
+    <link href="css/overrides.css" rel="stylesheet">
+	<link href="css/custom.css" rel="stylesheet">
 	<link href="css/img-hover-effect.css" rel="stylesheet"> 
 	<link href='http://fonts.googleapis.com/css?family=Open+Sans' rel='stylesheet' type='text/css'>
 	 
-</script>
 </head>
 <body>
-	 	
+	<!-- Facebook Likebox SDK -->
+	<div id="fb-root"></div>
+	<script>(function(d, s, id) {
+	  var js, fjs = d.getElementsByTagName(s)[0];
+	  if (d.getElementById(id)) return;
+	  js = d.createElement(s); js.id = id;
+	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=716149938482243&version=v2.0";
+	  fjs.parentNode.insertBefore(js, fjs);
+	}(document, 'script', 'facebook-jssdk'));</script> 	
+	<!-- end -->
+	
     <?php include('partials/header.html');?> 	
      
     <div class="main">
         <div class="container">
             <div class="row">
-                <div class="col-md-6 pull-left "> 
-					<div class="video-box"> </div>
-					<div class="video">
-							<iframe src="http://player.vimeo.com/video/21218268?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="560" height="310" style="border:0px" frameborder="0" allowtransparency="true"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
-					</div>
-                </div>
+               <div class="col-md-6 pull-left "> 
+						<div class="video-box"> </div>
+						<div class="video">
+								<iframe src="http://player.vimeo.com/video/21218268?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1" width="560" height="310" style="border:0px" frameborder="0" allowtransparency="true"  webkitallowfullscreen mozallowfullscreen allowfullscreen></iframe> 
+						</div>
+               </div>
 
                 <div class="col-md-6 pull-right mainText">
-                    <blockquote style="line-height: 1.8em;  ">
-                        We cover a wide variety of needs at Nashville K-9: Search and Rescue, Executive Protection, we even train dogs for law enforcement agencies. However the most important thing we do is address the needs of your house pets. From day one we implement techniques like luring, imprinting, and proofing to make sure your new family member starts out with a strong foundation of obedience and trainability. Our techniques are practical, fun, and will impress your neighbors. With a lifetime guarantee, you can rest assured that we stand behind our training.
-                    </blockquote>
+							<div class="row">
+								<div class="col-md-12" style="line-height: 1.8em; font-size:17px ">
+									  
+									  We cover a wide variety of needs at Nashville K-9: search and rescue, executive protection; we even train dogs for law enforcement agencies. However, the most important thing we do is address the needs of your house pets.  From day one we implement techniques like luring, imprinting, and proofing to make sure your new family member starts out with a strong foundation of obedience and trainability.  Our techniques are practical, fun, and will impress your neighbors.  With a lifetime guarantee, you can rest assured that we stand behind our training.
+							 
+								 </div>
+							</div>
+							
+							
                 </div>
             </div>
         </div>
