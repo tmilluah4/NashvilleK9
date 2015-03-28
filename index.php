2d5be6138cff58156273a9138005b650997fb0d3
tmilluah4 <todd.miller3@yahoo.com>
2015-03-28 13:50:05 -0500

    changes

--- index.php
+++ index.php
@@ -1,41 +1,35 @@
  
 <html>
 <head> 
-     <?php include('partials/head.html');?> 	  
-	    <style>
-  
-  </style>
+     <?php include('partials/head.html');?> 	   
 </head>
-<body>
-  
-	<!-- Facebook Likebox SDK -->
+<body> 
+ 
 	<div id="fb-root"></div>
-	<script>(function(d, s, id) {
-	  var js, fjs = d.getElementsByTagName(s)[0];
-	  if (d.getElementById(id)) return;
-	  js = d.createElement(s); js.id = id;
-	  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=716149938482243&version=v2.0";
-	  fjs.parentNode.insertBefore(js, fjs);
-	}(document, 'script', 'facebook-jssdk'));</script> 	
-	<!-- end -->
+	<script type="text/javascript">  
+		(function(d, s, id) {
+		  var js, fjs = d.getElementsByTagName(s)[0];
+		  if (d.getElementById(id)) return;
+		  js = d.createElement(s); js.id = id;
+		  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&appId=716149938482243&version=v2.0";
+		  fjs.parentNode.insertBefore(js, fjs);
+		}(document, 'script', 'facebook-jssdk'));
+	</script> 	
+ 
 	 <script type="text/javascript">  
 		function setActive()	{ 
 			 document.getElementById('homeli').className='active'; 
 		}
 		window.onload = setActive;
-	</script>
-  
-	
-   
-	<div class="container-fluid"  style="margin-top:40px; " >			
-		<div class="row grad"  >
-			<div class="col-md-2"></div>	
-			<div class="col-md-8"  >
-				<div class="row">
-					 <?php include('partials/header.html');?> 	 
-				</div>
-				<div class="row"> 	
-				 
+	</script> 
+    
+	<div class="container"  style="margin-top:40px;   box-shadow: 0px  0px 5px #333" >	 
+	  <?php include('partials/header.html');?> 	
+		<div class="row"  >
+		 
+			<div class="col-md-12"  >     
+			  
+				<div class="row main-row"> 	 
                <div class="col-md-6">  
 						<div class="embed-responsive embed-responsive-16by9" style=" -moz-box-shadow: 0 0 10px #000; -webkit-box-shadow: 0 0 10px #000; box-shadow: 0 0 10px #000; ">
 						  <iframe class="embed-responsive-item" src="http://player.vimeo.com/video/21218268?title=0&amp;byline=0&amp;portrait=0&amp;autoplay=1"></iframe>
@@ -49,9 +43,9 @@
 				<div class="row lower-main">
 					<?php include('partials/lower-main.html');?> 	
 				</div>
-        </div>
-		  <div class="col-md-2">   </div>
+         </div>
 		  
+		  </div>
     </div>	
 	
 	
