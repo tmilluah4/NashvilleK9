533ab0deeadb0cd6b08c69e16572ddcd7f67a24a
tmilluah4 <todd_miller@onlifehealth.com>
2015-04-02 21:52:54 -0500

    sdf

--- mailer.php
+++ mailer.php
@@ -1,26 +1,17 @@
 <?php
 require 'vendor/autoload.php';
-Dotenv::load(__DIR__);
-$user = 'app35497125@heroku.com'; // place SG username here
-   $pass = 'Liam4444'; // place SG password here
-$sendgrid_username = 'app35497125@heroku.com';
-$sendgrid_password = 'Liam4444';
-$to                = 'rtoddmiller3@gmail.com';
-
-$sendgrid = new SendGrid($sendgrid_username, $sendgrid_password, array("turn_off_ssl_verification" => true));
-$email    = new SendGrid\Email();
-$email->addTo($to)->
-       setFrom($to)->
-       setSubject('Please Work')->
-       setText('Owl are you doing?')->
-       setHtml('Please work for the love')->
-       addHeader('X-Sent-Using', 'SendGrid-API')->
-       addHeader('X-Transport', 'web')->
-       addAttachment('/gif.gif');
-
-$response = $sendgrid->send($email);
-var_dump($response);
-
  
+
+ require 'vendor/autoload.php';
+$sendgrid = new SendGrid('app35497125@heroku.com', '62osfefm');
+
+$message = new SendGrid\Email();
+$message->addTo('rtoddmiller3@gmail.com')->
+          setFrom('rtoddmiller3@gmail.com')->
+          setSubject('Subject goes here')->
+          setText('Hello World!')->
+          setHtml('<strong>Hello World!</strong>');
+$response = $sendgrid->send($message);
+?>
 
  
