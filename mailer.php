b0f9154486809fada07fc5320f0a1634323549f8
tmilluah4 <todd_miller@onlifehealth.com>
2015-04-02 22:31:36 -0500

    sdf

--- mailer.php
+++ mailer.php
@@ -1,21 +1,26 @@
 <?php
- print_r( $_POST );  
+  
 	require 'vendor/autoload.php';
   
 	$sendgrid = new SendGrid('app35497125@heroku.com', '62osfefm');
 
 	$message = new SendGrid\Email();
- print_r( $_POST ); 
+
 	$name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
     $message = $_POST['message'];
 
+
+ print_r( $name ); 
+ print_r( $email ); 
+ print_r( $subject ); 
+ print_r( $message );
+
 	$message->addTo('rtoddmiller3@gmail.com')->
           setFrom('rtoddmiller3@gmail.com')->
           setSubject('Contact Form Submission - Nashville K-9')->
           setText('Name: $name\n  Email: $email\n  Subject: $subject\n  $message')->
-          setHtml('<strong>Name: $name\n</br>  Email: $email\n</br>  Subject: $subject\n</br>  $message</strong>');
+          setHtml('<strong>Name: $name \n  Email: $email \n Subject: $subject \n   $message</strong>');
 	$response = $sendgrid->send($message);
-?>
-
+?> 
