26417bc243c49039dca670c276d9ac5c6b49993a
tmilluah4 <todd_miller@onlifehealth.com>
2015-04-02 22:46:12 -0500

    sdf

--- mailer.php
+++ mailer.php
@@ -11,18 +11,17 @@
 	$name = $_POST['name'];
     $email = $_POST['email'];
     $subject = $_POST['subject'];
-    $message = $_POST['message'];
-
+ 
 
  print_r( $name ); 
  print_r( $email ); 
  print_r( $subject ); 
- print_r( $message );
+ 
 
 $message->addTo('rtoddmiller3@gmail.com')->
           setFrom('rtoddmiller3@gmail.com')->
           setSubject('Contact Form Submission')->
-          setText('Name:'  $name ' \n  Email:'    $email '\n  Subject:'  $message)->
-          setHtml('<strong>Name:  ' $name '\n  Email:'   $email '\n Subject:'    $message '</strong>');
+          setText('Name: $name\n  Email: $email\n  Subject: $subject\n   ')->
+          setHtml('<strong>Name: $name \n  Email: $email \n Subject: $subject \n   </strong>');
 $response = $sendgrid->send($message);
 ?>
