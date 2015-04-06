e420ae9387d2a92cd9906c005cf7081c2ca1fbf8
tmilluah4 <todd_miller@onlifehealth.com>
2015-04-06 16:47:32 -0500

    sdfs

--- training-videos.php
+++ training-videos.php
@@ -1,11 +1,11 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<?php include('/partials/head.html');?>
+	<?php include('partials/head.html');?>
 <style>
 	.switcher-content { display: none;}
 </style>
-<script type="text/javascript" src="/js/jquery.content-panel-switcher.js"></script>
+<script type="text/javascript" src="js/jquery.content-panel-switcher.js"></script>
 <script type="text/javascript">
 	$(document).ready(function() {
 		jcps.fader(300, '#switcher-panel');
@@ -28,7 +28,7 @@
 		<div class="row">
 			<div class="col-sm-1"> </div>
 			<div class="col-sm-10">
-				<?php include('/partials/header.html');?>
+				<?php include('partials/header.html');?>
 				<div class="row main-row">
 					<div class="col-sm-4">
 						<div class="image-random">
@@ -39,7 +39,7 @@
 						</br>
 						<ul  class="list-group" style="cursor: pointer;">
 							<li id="content1" class="list-group-item switcher active"> WHY WE ARE BETTER! </li>
-							<li id="content2" class="list-group-item switcher"> OUR CLIENTS ARE SAYING</li>
+							<li id="content2" class="list-group-item switcher"> WHAT OUR CLIENTS ARE SAYING</li>
 							<li id="content3" class="list-group-item switcher"> SOME OF OUR DOGS</li>
 							<li id="content4" class="list-group-item switcher"> DOG OBEDIENCE</li>
 							<li id="content5" class="list-group-item switcher">FOUR WEEK BOARD AND TRAIN</li>
@@ -57,55 +57,55 @@
 						<div class="section-with-header"> 
 							<!-- VIEW -->
 							<div id="switcher-panel"  >
-								<?php include('/videos/why-we-are-better.html');?>
+								<?php include('videos/why-we-are-better.html');?>
 							</div>
 							<!-- WHY WE ARE BETTER -->
 							<div id="content1-content" class="switcher-content ">
-								<?php include('/videos/why-we-are-better.html');?>
+								<?php include('videos/why-we-are-better.html');?>
 							</div>							
 							<!-- CLIENTS -->
 							<div id="content2-content" class="switcher-content">
-								<?php include('/videos/what-clients-are-saying.html');?>
+								<?php include('videos/what-clients-are-saying.html');?>
 							</div>							
 							<!-- SOME OF OUR DOGS -->
 							<div id="content3-content" class="switcher-content">
-								<?php include('/videos/some-of-our-dogs.html');?>
+								<?php include('videos/some-of-our-dogs.html');?>
 							</div>
 							<!-- DOG OBEDIENCE -->
 							<div id="content4-content" class="switcher-content">
-								<?php include('/videos/dog-obedience.html');?>
+								<?php include('videos/dog-obedience.html');?>
 							</div>
 							<!-- 4 WEEK BOARD AND TRAIN -->
 							<div id="content5-content" class="switcher-content">
-								<?php include('/videos/four-week-board-and-train.html');?>
+								<?php include('videos/four-week-board-and-train.html');?>
 							</div>
 							<!-- DOG PROTECTION -->
 							<div id="content6-content" class="switcher-content">
-								<?php include('/videos/dog-protection.html');?>
+								<?php include('videos/dog-protection.html');?>
 							</div>
 							<!-- SEARCH AND RESCUE -->
 							<div id="content7-content" class="switcher-content">
-								<?php include('/videos/search-and-rescue.html');?>
+								<?php include('videos/search-and-rescue.html');?>
 							</div>
 							<!-- DOG AGILITY -->
 							<div id="content8-content" class="switcher-content">
-								<?php include('/videos/dog-agility.html');?>
+								<?php include('videos/dog-agility.html');?>
 							</div>
 							<!-- DEMONSTRATIONS -->
 							<div id="content9-content" class="switcher-content">
-								<?php include('/videos/demonstrations.html');?>
+								<?php include('videos/demonstrations.html');?>
 							</div>
 							<!-- PET FIRST AID -->
 							<div id="content10-content" class="switcher-content">
-								<?php include('/videos/pet-first-aid.html');?>
+								<?php include('videos/pet-first-aid.html');?>
 							</div>
 							<!-- RING SPORT -->
 							<div id="content11-content" class="switcher-content">
-								<?php include('/videos/ring-sport.html');?>
+								<?php include('videos/ring-sport.html');?>
 							</div>
 							<!-- COMMERCIALS -->
 							<div id="content12-content" class="switcher-content">
-								<?php include('/videos/commercials.html');?>
+								<?php include('videos/commercials.html');?>
 							</div>
 						</div>
 					</div>
@@ -115,7 +115,7 @@
 		</div> 
 	</div>
 
-	<?php include('/partials/footer.html');?>
+	<?php include('partials/footer.html');?>
 
 </body>
 </html> 
